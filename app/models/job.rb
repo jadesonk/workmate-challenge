2e76d60b104ac87557e9f2302f59474793a24743
jadeson komolvipat <jadesonk@gmail.com>
2020-10-19 15:29:55 +0700

    setup associations and validations for models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,6 @@
 class Job < ApplicationRecord
+  has_many :shifts
+  has_many :users, through: :shifts
+
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :shifts
+  # I am assuming that a user can have multiple jobs for now
+  has_many :jobs, through: :shifts
+
+  validates :name, presence: true
 end
